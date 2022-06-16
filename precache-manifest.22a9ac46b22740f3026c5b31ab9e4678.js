2f3b81262fb8e465482e8189a62205e7784191bd
madisoncarsan <madisoncarsan@gmail.com>
2022-06-15 21:10:15 -0600

    Updates

---
+++ precache-manifest.22a9ac46b22740f3026c5b31ab9e4678.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e196d26bc17ade4c602ac6bea363c6d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fd5a32b79fb2d3b24cf1",
+    "url": "/static/css/3.dbccd085.chunk.css"
+  },
+  {
+    "revision": "fa4544ef01d377180b95",
+    "url": "/static/css/4.29c568fc.chunk.css"
+  },
+  {
+    "revision": "700b8ec18f2075b0eeba",
+    "url": "/static/css/main.a7f09177.chunk.css"
+  },
+  {
+    "revision": "cab6a0695c18b1691c95",
+    "url": "/static/js/2.302d1d63.chunk.js"
+  },
+  {
+    "revision": "6e1d2efc12a1ac972ff1f942df7eb0bd",
+    "url": "/static/js/2.302d1d63.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fd5a32b79fb2d3b24cf1",
+    "url": "/static/js/3.018d5c8a.chunk.js"
+  },
+  {
+    "revision": "fa4544ef01d377180b95",
+    "url": "/static/js/4.8f282b63.chunk.js"
+  },
+  {
+    "revision": "700b8ec18f2075b0eeba",
+    "url": "/static/js/main.c7b5c022.chunk.js"
+  },
+  {
+    "revision": "be20928ab161cd0b161e",
+    "url": "/static/js/runtime-main.730bad98.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "3f2559d5d5a90dbdf9b11a59acef41ca",
+    "url": "/static/media/CV-logo.3f2559d5.webp"
+  },
+  {
+    "revision": "1a40c94b6a92d4a0693ab27e7ec147dd",
+    "url": "/static/media/HH-logo1.1a40c94b.png"
+  },
+  {
+    "revision": "8f4e3cc2b5d52354a6d43c8edba1e3c9",
+    "url": "/static/media/IBM-logo.8f4e3cc2.svg"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "1519d9e1d5b00a6669b9dfcc25067180",
+    "url": "/static/media/Surprised_Pikachu.1519d9e1.png"
+  },
+  {
+    "revision": "5cb30d6162d4ebf7ca9f0005e1d70f63",
+    "url": "/static/media/Wine.5cb30d61.jpg"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "153e0d992fd31d05a848ffe7c408327d",
+    "url": "/static/media/cornell-logo.153e0d99.png"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "82dbf58f0ad6abcf7e01a3268492bafa",
+    "url": "/static/media/google-cert.82dbf58f.png"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "d0af2e84badc4c15838ba080a847bb97",
+    "url": "/static/media/sol.d0af2e84.png"
+  },
+  {
+    "revision": "13780cdd0144cacdef25486e2c2503f8",
+    "url": "/static/media/talksCardBack.13780cdd.svg"
+  }
+]);
